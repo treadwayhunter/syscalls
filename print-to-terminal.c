2d9550fe4056e9084f51298bb26fe0c9876fd09f
Hunter Treadway <treadwayhunter@gmail.com>
2024-06-18 21:07:48 -0500

    Got integers working in my print function.

--- print-to-terminal.c
+++ print-to-terminal.c
@@ -3,15 +3,27 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-void treadprint(char *msg, ...) { // from here, I can do all sorts.
-    //write(STDOUT_FILENO, msg, strlen(msg));
+// not elegant, but fast.
+int numPlaces(int n) {
+    if (n < 0) return numPlaces(-n) + 1;
+    if (n < 10) return 1;
+    if (n < 100) return 2;
+    if (n < 1000) return 3;
+    if (n < 10000) return 4;
+    if (n < 100000) return 5;
+    if (n < 1000000) return 6;
+    if (n < 10000000) return 7;
+    if (n < 100000000) return 8;
+    if (n < 1000000000) return 9;
+    return 10;
+}
 
-    // editting the same message I am iterating through sounds dangerous.
+void treadprint(char *msg, ...) {
+
     char buffer[1024];
     int buffer_loc = 0;
     va_list args;
     va_start(args, msg);
-
     int num_args = 0;
 
     // use ':' as a delimeter for format
@@ -22,25 +34,32 @@ void treadprint(char *msg, ...) { // from here, I can do all sorts.
             switch(*p) {
                 case 'i': {
                     int i = va_arg(args, int);
-                    printf("The given int: %d\n", i);
+                    char temp[16];
+                    sprintf(temp, "%d", i);
+                    for (int c = 0; c < numPlaces(i); c++) {
+                        buffer[buffer_loc] = temp[c];
+                        buffer_loc++;
+                    }
                     break; 
                 }
+                /**
+                 * Add other cases for floats, doubles, strings, etc.
+                 */
             }
         }
         else {
-            buffer[buffer_loc] = p;
+            buffer[buffer_loc] = *p;
+            buffer_loc++;
         }
     }
-    // let's make it take integers
     va_end(args);
-    printf("Num args: %d\n", num_args);
-    write(STDOUT_FILENO, msg, strlen(msg));
+
+    char new_msg[buffer_loc];
+    strcpy(new_msg, buffer);
+    write(STDOUT_FILENO, new_msg, sizeof(new_msg));
 }
 
 int main() {
-    //char buffer[10] = "";
-    //buffer[0] = 'A';
-    //buffer[1] = 'B';
-    //printf("%s\n", buffer);
-    //treadprint("Hello World :i\n", 5);
+
+    treadprint("Can I handle negative numbers? :i \n", -100);
 }
